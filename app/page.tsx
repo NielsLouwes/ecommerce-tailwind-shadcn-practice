234b8a070e08bcf1400b19f8aa4aee8c5ba8f8cc
Niels Louwes <nielslouwes45@gmail.com>
2024-02-06 16:26:59 +0100

    added-product-image-selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,47 @@
+"use client";
+
 import Header from "@/components/Header";
 import ProductInfoCard from "@/components/ProductInfoCard";
 import Image from "next/image";
+import { useState } from "react";
+
+const productImages = [
+  { id: 1, image: "/image-product-1.jpg" },
+  { id: 2, image: "/image-product-2.jpg" },
+  { id: 3, image: "/image-product-3.jpg" },
+  { id: 4, image: "/image-product-4.jpg" },
+];
 
 export default function Home() {
-  const productImages = [
-    { id: 1, image: "/image-product-1.jpg" },
-    { id: 2, image: "/image-product-2.jpg" },
-    { id: 3, image: "/image-product-3.jpg" },
-    { id: 4, image: "/image-product-4.jpg" },
-  ];
+  const [selectedImage, setSelectedImage] = useState(productImages[0]);
+
+  const selectImage = (id: number) => {
+    const findImage = productImages.find((item) => id === item.id);
+    setSelectedImage(findImage); 
+  };
 
   return (
     <main>
       <Header />
-      <div className="md:container md:mx-auto flex border-solid border-2 border-purple-600 gap-24 px-8 2xl:px-16 mt-12">
+      <div className="md:container md:mx-auto flex gap-24 px-8 2xl:px-16 mt-12">
         <div className="flex flex-col">
-          <div className="rounded-md overflow-hidden pt-8">
-            <Image
-              src="/image-product-1.jpg"
-              width={500}
-              height={500}
-              alt="picture of sneakers"
-            />
+          <div className="rounded-md overflow-hidden pt-8 mb-4">
+            {selectedImage ? (
+              <Image
+                src={selectedImage.image}
+                width={500}
+                height={500}
+                alt="picture of sneakers"
+              />
+            ) : null}
           </div>
-          <div className="flex flex-row gap-8 pb-8"> 
-            {productImages.map(({ id, image }) => (
-              <div key={id} className="rounded-md overflow-hidden">
+          <div className="flex flex-row gap-8 pb-8 cursor-pointer">
+            {productImages.map(({ image, id }) => (
+              <div
+                key={id}
+                onClick={() => selectImage(id)}
+                className="rounded-md overflow-hidden"
+              >
                 <Image
                   src={image}
                   width={100}
@@ -47,5 +63,3 @@ export default function Home() {
     </main>
   );
 }
-
-// src="/images/logo-reclamefolder.png"
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <main>
       <div className="flex gap-20 h-20 border-solid border-black bg-black items-center">
         <h2 className="text-2xl font-bold">Sneakers</h2>
-        <div className="flex gap-6 text-gray-300">
+        <div className="flex gap-6 text-gray-300 cursor-pointer">
           {categories.map((category) => (
             <p key={category}>{category}</p>
           ))}
